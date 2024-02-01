3faae4dfd56b2a8b88f6d089ea7dac402cc5de7b
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2024-02-01 13:13:22 +0800

    feat: add database_name constructor

--- activities/01/db_connection.py
+++ activities/01/db_connection.py
@@ -2,34 +2,37 @@ from mysql.connector import connect, Error
 
 
 class Connection:
-    def __init__(self, username, password, host):
+    # Initialize constructor
+    def __init__(self, username, password, database, host):
         self.username = username
         self.password = password
+        self.database = database
         self.host = host
 
-    def establish_connection(self):
-        connect(username=self.username, password=self.password, host=self.host)
-        print('Successfully connected')
+    def establish_connection(self, database_name):
+        connection = connect(user=self.username, password=self.password, database=database_name,
+                             host=self.host)
+        # print('Successfully connected')
+        return connection
 
     def create_database(self, database_name):
-        try:
-            connection = connect(username=self.username, password=self.password, host=self.host)
-            cursor = connection.cursor()
-            cursor.execute(f'CREATE DATABASE {database_name}')
-            print(f'Successfully created {database_name}')
-        except Error as e:
-            print(e.msg)
-
-
-def create_table(connection, database_name, table_name):
-    try:
+        connection = self.establish_connection(database_name='')
         cursor = connection.cursor()
-        cursor.execute(f'USE {database_name};')
-        cursor.execute(f'CREATE TABLE {table_name} (id INT PRIMARY KEY);')
+        cursor.execute(f'CREATE DATABASE {database_name}')
+        connection.commit()
+        print(f'Successfully created {database_name}')
 
-        print(f'Table {table_name} created successfully!')
-    except Error as error:
-        print(error)
+    def create_table(self, database_name, table_name):
+        connection = self.establish_connection(database_name=database_name)
+
+        cursor = connection.cursor()
+        cursor.execute(f"""
+            CREATE TABLE {table_name} (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                name VARCHAR(40) NOT NULL
+            );
+        """)
+        connection.commit()
 
 
 def insert_data(connection, database_name, table_name, data):
--- activities/01/main.py
+++ activities/01/main.py
@@ -4,35 +4,44 @@ from db_connection import Connection
 
 # Declare constant variables
 USERNAME = 'root'
-PASSWORD = '1234567890'
+PASSWORD = '1234567890'  # Change this to `root` or whatever password you set during installation
+DATABASE = ''
 HOST = 'localhost'
 
 try:
     # Create an instance for the Connection class
-    connection = Connection(username=USERNAME, password=PASSWORD, host=HOST)
+    connection = Connection(username=USERNAME, password=PASSWORD, database='', host=HOST)
 
     # Connect to the database
-    connection.establish_connection()
+    connection.establish_connection('')
 
-    # Print options
-    print('Welcome to IPTC312')
-    print('Choose from the following options:')
-    print('1. Create a database')
-    print('2. Create a table')
-    print('3. Insert data into a table')
-    print('4. Update data in a table')
-    print('5. Delete data from a table')
-    print('6. Drop a table')
-    print('7. Select data from a table')
-    print('8. Exit\n')
+    # Continuously run the program after each transaction
+    while True:
+        # Print options
+        print('Welcome to IPTC312')
+        print('Choose from the following options:')
+        print('1. Create a database')
+        print('2. Create a table')
+        print('3. Insert data into a table')
+        print('4. Update data in a table')
+        print('5. Delete data from a table')
+        print('6. Drop a table')
+        print('7. Select data from a table')
+        print('8. Exit\n')
 
-    # Ask the user
-    option = int(input('Enter your choice: '))
+        # Ask the user
+        option = int(input('Enter your choice: '))
 
-    if option == 1:
-        database_name = input('Enter the name of the database: ')
-        connection.create_database(database_name=database_name)
-    elif option == 8:
-        print('Exit')
+        if option == 1:
+            database_name = input('Enter the name of the database: ')
+            # Create a database
+            connection.create_database(database_name=database_name)
+            DATABASE = database_name
+        if option == 2:
+            table_name = input('Enter the name of the table: ')
+            # Create a table
+            connection.create_table(database_name=DATABASE, table_name=table_name)
+        elif option == 8:
+            break
 except Error as e:
     print('An error occurred:', e.msg)
