f99e656fc5030915c7b108c0f52519c60626f6bc
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2024-02-03 16:10:06 +0800

    style: update program feedback

--- activities/01/db_connection.py
+++ activities/01/db_connection.py
@@ -1,6 +1,10 @@
 from mysql.connector import connect
 
 
+def log(string):
+    print(f'\n{string}\n')
+
+
 class Connection:
     # Initialize constructor
     def __init__(self, username, password, database, host):
@@ -19,7 +23,7 @@ class Connection:
         cursor = connection.cursor()
         cursor.execute(f'CREATE DATABASE {database_name};')
         connection.commit()
-        print(f'Successfully created {database_name}')
+        log(f'Successfully created a database named {database_name}.')
 
     def create_table(self, database_name, table_name):
         connection = self.establish_connection(database_name=database_name)
@@ -33,33 +37,35 @@ class Connection:
             );
             """)
         connection.commit()
+        log(f'Successfully created a table named {table_name} under the {database_name} database.')
 
     def insert_data(self, database_name, table_name, data):
         connection = self.establish_connection(database_name=database_name)
         cursor = connection.cursor()
         cursor.executemany(f'INSERT INTO {table_name} (name) VALUES (%s);', data)
         connection.commit()
-        print(f'\nInserted {len(data)} row(s) of data.\n')
+        log(f'Successfully inserted {len(data)} row(s) of data.')
 
-    def update_data(self, database_name, table_name):
+    def update_data(self, database_name, table_name, data_id, new_data):
         connection = self.establish_connection(database_name=database_name)
         cursor = connection.cursor()
-        cursor.execute(f'')
+        cursor.execute(f'UPDATE {table_name} SET name = "{new_data}" WHERE id = {data_id};')
         connection.commit()
+        log(f'Successfully updated a row.')
 
     def delete_data(self, database_name, table_name, data_id):
         connection = self.establish_connection(database_name=database_name)
         cursor = connection.cursor()
         cursor.execute(f'DELETE FROM {table_name} WHERE id = {data_id};')
         connection.commit()
-        print(f'\nSuccessfully deleted data with an id of {data_id}.\n')
+        log(f'Successfully deleted data with an id of {data_id}.')
 
     def drop_table(self, database_name, table_name):
         connection = self.establish_connection(database_name=database_name)
         cursor = connection.cursor()
         cursor.execute(f'DROP TABLE {table_name};')
         connection.commit()
-        print(f'\nDropped table {table_name} successfully.\n')
+        log(f'Successfully dropped the {table_name} table.')
 
     def select_data(self, database_name, choice, table_name):
         connection = self.establish_connection(database_name=database_name)
@@ -71,9 +77,10 @@ class Connection:
         if choice.lower() == 'y':
             for row in cursor.fetchall():
                 print(row)
+
+            log('Successfully displayed all data.')
         elif choice.lower() == 'n':
             print(cursor.fetchone())
+            log('Successfully displayed a data.')
         else:
-            print('Incorrect input. Please try again.')
-
-        print()
+            log('Incorrect input. Please try again.')
