17578d287d47e6d89bc1c08e1c35f23fa2c10011
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2024-02-18 18:30:23 +0800

    feat: create center_window utility

---
+++ playground/login/center_window.py
@@ -0,0 +1,6 @@
+def center_window(width, height, window):
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x = (screen_width / 2) - (width / 2)
+    y = (screen_height / 2) - (height / 2)
+    window.geometry('%dx%d+%d+%d' % (width, height, x, y))
