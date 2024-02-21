9059df92236475a093f90146839a76cef8b4f5fc
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2024-02-21 22:10:37 +0800

    feat: create hash_ and verify_password functions

--- playground/login/connection.py
+++ playground/login/connection.py
@@ -1,3 +1,5 @@
+import hashlib
+
 from mysql.connector import connect, Error
 
 
@@ -10,30 +12,45 @@ class Connection:
 
     def establish_connection(self):
         try:
-            connection = connect(user=self.username, password=self.password, database=self.database,
-                                 host=self.host)
-            return connection
+            return connect(user=self.username, password=self.password, database=self.database,
+                           host=self.host)
         except Error as e:
             print(e)
+            return None
 
     def sign_up(self, username, password):
-        connection = self.establish_connection()
-        cursor = connection.cursor()
-        query = 'INSERT INTO Employees (username, password) VALUES (%s, %s);'
+        with self.establish_connection() as connection:
+            if connection is None:
+                return
 
-        cursor.execute(query, (username, password,))
-        connection.commit()
-        print('Data inserted')
+            cursor = connection.cursor()
+            hashed_password = self.hash_password(password)
+            query = 'INSERT INTO Employees (username, password) VALUES (%s, %s);'
+            cursor.execute(query, (username, hashed_password,))
+            connection.commit()
+            print('Data inserted')
 
     def log_in(self, username, password):
-        connection = self.establish_connection()
-        cursor = connection.cursor()
-        query = 'SELECT * FROM Employees WHERE username = %s AND password = %s;'
+        with self.establish_connection() as connection:
+            if connection is None:
+                return
 
-        cursor.execute(query, (username, password,))
-        data = cursor.fetchall()
+            cursor = connection.cursor()
+            query = 'SELECT * FROM Employees WHERE username = %s;'
+            cursor.execute(query, (username,))
+            data = cursor.fetchall()
 
-        if len(data) != 0:
-            print('Log in successful')
-        else:
-            print('Log in failed')
+            if len(data) == 0:
+                print('Log in failed')
+            elif self.verify_password(password, data[0][1]):
+                print('Log in successful')
+            else:
+                print('Log in failed')
+
+    @staticmethod
+    def hash_password(password):
+        return hashlib.sha256(password.encode()).hexdigest()
+
+    @staticmethod
+    def verify_password(password, hashed_password):
+        return hashlib.sha256(password.encode()).hexdigest() == hashed_password
